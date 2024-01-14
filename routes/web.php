24089f0b5587da996d984cafb113bb7c7f190e6f
Baloot <baloot.mhl@gmail.com>
2024-01-15 00:10:24 +0630

    installed amazonpay-sdk package

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
+
+
+Route::get('/payments', [App\Http\Controllers\HomeController::class, 'payments'])->name('payments');
