ba79bbddc9b6f675d78a01249c54cb9992c10279
Baloot <baloot.mhl@gmail.com>
2024-02-13 23:16:54 +0630

    disabled register route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
     return redirect()->route('home');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
